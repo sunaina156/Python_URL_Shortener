d17675719e6e7e1887d7087a2dd8b3c550d577d3
sunaina156 <sunainalodha475@gmail.com>
2026-09-17 23:43:10 +0530

    added frontend files; index.html, javascript.js, style.css

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from fastapi.responses import RedirectResponse
 from db import get_connection
 from models import URLCreate, ErrorResponse
 
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create the FastAPI application:
 
@@ -196,3 +197,14 @@ def redirect_to_original_url(
     finally:
         cursor.close()
         connection.close()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
