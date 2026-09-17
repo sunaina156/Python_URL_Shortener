4562e8d50a2b9cad5729ef8f6f05a40f1ecb10e9
sunaina156 <sunainalodha475@gmail.com>
2026-09-17 19:03:41 +0530

    Added automatic testing for fastapi

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+
+client = TestClient(app)
+
+
+def test_home_endpoint():
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    assert response.json() == {
+        "message": "URL Shortener API is running"
+    }
